3647155cf958f0dd108452e4cf1d5871188612c8
harry.qian <codingqst@gmail.com>
2017-03-28 10:04:13 +0800

    Initial Commit

---
+++ W/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W
+//
+//  Created by harry.qian on 17/3/28.
+//  Copyright © 2017年 harry.qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
